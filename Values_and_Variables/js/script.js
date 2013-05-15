7f326879825986d15e0753cf1aedd1bb2ec6cfb7
Andrew Clark <akclark@fullsail.edu>
2013-05-15 01:23:52 -0400

    String and Boolean Variables sub-lesson.

--- Values_and_Variables/js/script.js
+++ Values_and_Variables/js/script.js
@@ -1,7 +1,10 @@
 // JavaScript Document
-//Defining Variables
-var age; //declaration
-age = 36; //definition
-//storage container = value
+//Variables of Different Types, Numbers, Boolean and Strings
 
-var yearOfBirth = 1976;  //declaring and defining on the same line
+var age = 36; //Number variable
+
+var isStudent = false; //Boolean Variable
+
+var firstName = "Andrew";
+
+var phrase = 'I don\'t know \n You\'ll have to speak with the professor';
