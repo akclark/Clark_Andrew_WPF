2b93ee89f2d6c588e54624538e3ea8b838c699c9
Andrew Clark <akclark@fullsail.edu>
2013-05-15 01:12:10 -0400

    Defining variables sub-lesson

--- Values_and_Variables/js/script.js
+++ Values_and_Variables/js/script.js
@@ -1,3 +1,7 @@
 // JavaScript Document
-//Declaring Variables
-var age;
+//Defining Variables
+var age; //declaration
+age = 36; //definition
+//storage container = value
+
+var yearOfBirth = 1976;  //declaring and defining on the same line
