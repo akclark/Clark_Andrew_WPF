c1f295138de0c90efd1c73c0badf04027fc4885e
Andrew Clark <akclark@fullsail.edu>
2013-05-16 16:57:04 -0400

    Expressions Worksheet - Slice of Pie Part I addition

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -12,3 +12,14 @@ console.log("The area of the rectangle is " + area);
 var sparkyAge = 12;
 var sparkyDogYears = sparkyAge * 7;
 console.log("Sparky is " + sparkyAge + " human years old, which is " + sparkyDogYears + " in dog years.");
+
+//Slice of Pie Part I
+
+var slicesPerPie = 8;
+var numberOfGuests = 34;
+var piesOrdered = 23;
+
+var slicesPerGuest = piesOrdered * slicesPerPie / numberOfGuests;
+ 
+console.log("Each person ate " + slicesPerGuest + " slices of pizza at the party.");
+
