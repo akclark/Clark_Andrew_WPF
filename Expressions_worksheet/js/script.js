c08cb28c0a9cbf71ccecc50c1a5c6907a1f9ea3c
Andrew Clark <akclark@fullsail.edu>
2013-05-16 16:49:32 -0400

    Expressions Worksheet - Dog Years Problem addition

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -7,4 +7,8 @@ var area = width * height;
 
 console.log("The area of the rectangle is " + area);
 
+//Dog Years Calculator
 
+var sparkyAge = 12;
+var sparkyDogYears = sparkyAge * 7;
+console.log("Sparky is " + sparkyAge + " human years old, which is " + sparkyDogYears + " in dog years.");
