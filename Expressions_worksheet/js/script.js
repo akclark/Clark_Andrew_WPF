43d10b6c775d95e6e0c563bacf53df9d22aff662
Andrew Clark <akclark@fullsail.edu>
2013-05-16 16:48:01 -0400

    Initial setup and Example problem.

---
+++ Expressions_worksheet/js/script.js
@@ -0,0 +1,10 @@
+// Andrew K Clark - 05/16/2013 - Expression Worksheet
+
+//Example Problem
+var width = 8;
+var height = 26;
+var area = width * height;
+
+console.log("The area of the rectangle is " + area);
+
+
