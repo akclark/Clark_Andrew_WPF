7b963046ceacef8e227f370ab892cc5618ce00f5
Andrew Clark <akclark@fullsail.edu>
2013-05-16 19:03:21 -0400

    Added comments to explain the variable and user input section.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -2,7 +2,7 @@
 
 //kilowatt - hour conversion per number of days for given watt/hours/days per month
 
-var wattsPerHour = prompt("This will calculate the number of kWh (kiloWatt-hours) a device will use across a given number of days. \nPlease enter the number of watts your device uses:"); 
-var hoursPerDay = prompt("Please enter the average number of hours per day the device is used:");
-var daysDeviceUsed = prompt("Please enter the number of days you use this device:");
+var wattsPerHour = prompt("This will calculate the number of kWh (kiloWatt-hours) a device will use across a given number of days. \nPlease enter the number of watts your device uses:");  //This is where I explain the computation and first variable, and ask for the user to input the variable.
+var hoursPerDay = prompt("Please enter the average number of hours per day the device is used:"); //This is the second variable setup and input by the user.
+var daysDeviceUsed = prompt("Please enter the number of days you use this device:"); //This is the third variable setup and user input.
 
