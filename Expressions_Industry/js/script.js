fff989cd056b41bbf3b65001fa5bcfd1f3d72d59
Andrew Clark <akclark@fullsail.edu>
2013-05-16 23:27:03 -0400

    Added a couple lines to the beginning of the code asking for type of device being tested

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -2,7 +2,11 @@
 
 //kilowatt - hour conversion per number of days for given watt/hours/days per month, this can be useful for finding out how much a bulb or other device is adding to your electric bill.
 
-var wattsPerHour = prompt("This will calculate the number of kWh (kiloWatt-hours) a device will use across a given number of days. \nPlease enter the number of watts your device uses:");  //This is where I explain the computation and first variable, and ask for the user to input the variable.
+prompt("This will calculate the number of kWh (kiloWatt-hours) a device will use across a given number of days.");
+
+var deviceType = prompt("What type of device are we talking about here?");
+
+var wattsPerHour = prompt("Please enter the number of watts your device uses:");  //This is where I explain the computation and first variable, and ask for the user to input the variable.
 var hoursPerDay = prompt("Please enter the average number of hours per day the device is used:"); //This is the second variable setup and input by the user.
 var daysDeviceUsed = prompt("Please enter the number of days you use this device:"); //This is the third variable setup and user input.
 
