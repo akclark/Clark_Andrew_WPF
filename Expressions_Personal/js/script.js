dae5138b124b4062ae76142da203402a1f8f122b
Andrew Clark <akclark@fullsail.edu>
2013-05-16 23:00:43 -0400

    Added comments to first section

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,12 +1,12 @@
 // Andrew K Clark - 05/16/2013 - Expressions Personal
 
 var phoneBills = []
-phoneBills[0] = parseFloat(prompt("This will total and average the last 6 months worth of your landline or cell phone bills.\nOr any other bill that repeats on a monthly basis. \nPlease enter the amount of the first bill:"));
-phoneBills[1] = parseFloat(prompt("Please enter the second bill amount:"));
-phoneBills[2] = parseFloat(prompt("Please enter the third bill amount:"));
-phoneBills[3] = parseFloat(prompt("Please enter the fourth bill  amount:"));
-phoneBills[4] = parseFloat(prompt("Please enter the fifth bill amount:"));
-phoneBills[5] = parseFloat(prompt("And please enter the sixth and final bill amount:"));
+phoneBills[0] = parseFloat(prompt("This will total and average the last 6 months worth of your landline or cell phone bills.\nOr any other bill that repeats on a monthly basis. \nPlease enter the amount of the first bill:"));//This line describes the function and allow the first user input
+phoneBills[1] = parseFloat(prompt("Please enter the second bill amount:")); //Asking for second number to be placed into the array
+phoneBills[2] = parseFloat(prompt("Please enter the third bill amount:")); //Asking for third number to be placed into the array
+phoneBills[3] = parseFloat(prompt("Please enter the fourth bill  amount:")); //Asking for fourth number to be placed into the array
+phoneBills[4] = parseFloat(prompt("Please enter the fifth bill amount:")); //Asking for fifth number to be placed into the array
+phoneBills[5] = parseFloat(prompt("And please enter the sixth and final bill amount:")); //Asking for sixth number to be placed into the array
 
 var billsTotal = phoneBills[0] + phoneBills[1] + phoneBills[2] + phoneBills[3] + phoneBills[4] + phoneBills[5];
 var billsAverage = billsTotal / 6;
