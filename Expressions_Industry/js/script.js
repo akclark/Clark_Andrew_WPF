ec6f822ddafcd0ee9c74198b373b7215c13c4abb
Andrew Clark <akclark@fullsail.edu>
2013-05-16 21:17:28 -0400

    Added calculation lines and comments for those lines.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,8 +1,14 @@
-// Andrew K Clark - 05/16/2013 - Expressions Industry (Electrical)
+// Andrew K Clark - 05/16/2013 - Expressions Industry (Electrical, I am currently an electrician)
 
-//kilowatt - hour conversion per number of days for given watt/hours/days per month
+//kilowatt - hour conversion per number of days for given watt/hours/days per month, this can be useful for finding out how much a bulb or other device is adding to your electric bill.
 
 var wattsPerHour = prompt("This will calculate the number of kWh (kiloWatt-hours) a device will use across a given number of days. \nPlease enter the number of watts your device uses:");  //This is where I explain the computation and first variable, and ask for the user to input the variable.
 var hoursPerDay = prompt("Please enter the average number of hours per day the device is used:"); //This is the second variable setup and input by the user.
 var daysDeviceUsed = prompt("Please enter the number of days you use this device:"); //This is the third variable setup and user input.
+
+var totalWattHours = wattsPerHour * hoursPerDay; //Total watts per hour calculation and variable setup
+var totalKWH = totalWattHours /1000;  //Total kiloWatt-hours used calculation and variable setup
+var kwhPerDays = totalKWH * daysDeviceUsed; //final calculation for kiloWatt hours across the number of days the device is used.
+
+
 
