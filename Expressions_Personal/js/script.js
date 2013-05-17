2e4dd73184bcb89c65960d180c0c466786d3131d
Andrew Clark <akclark@fullsail.edu>
2013-05-16 22:05:40 -0400

    An experiment to see if I could use prompt to add things to an array appropriately.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,1 +1,6 @@
-// Andrew K Clark - 05/16/2013 - Expressions Personal 
+// Andrew K Clark - 05/16/2013 - Expressions Personal
+
+var phoneBills = []
+phoneBills[0] = prompt("This will total and average the last 6 months worth of your landline or cell phone bills.\nOr any other bill that repeats on a monthly basis. \nPlease enter the amount of the first bill:");
+
+console.log(phoneBills[0]);
