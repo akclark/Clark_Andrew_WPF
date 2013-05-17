8d7573dd775754c04e0d8dc7345e27f00cc95a8d
Andrew Clark <akclark@fullsail.edu>
2013-05-16 23:10:34 -0400

    Added a line to as the user for the type of bill being calculated

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,7 +1,8 @@
 // Andrew K Clark - 05/16/2013 - Expressions Personal
 
 var phoneBills = []
-phoneBills[0] = parseFloat(prompt("This will total and average the last 6 months worth of your landline or cell phone bills.\nOr any other bill that repeats on a monthly basis. \nPlease enter the amount of the first bill:"));//This line describes the function and allow the first user input
+var billName = prompt("This will total and average the last 6 months worth of your landline or cell phone bills.\nOr any other bill that repeats on a monthly basis. \nWhat type of bill is this (phone, water, etc...)?"); //This line describes the function and asks the user for the type of bill being calculated
+phoneBills[0] = parseFloat(prompt("Please enter the amount of the first bill:"));// Asking for the first number to be placed into the array
 phoneBills[1] = parseFloat(prompt("Please enter the second bill amount:")); //Asking for second number to be placed into the array
 phoneBills[2] = parseFloat(prompt("Please enter the third bill amount:")); //Asking for third number to be placed into the array
 phoneBills[3] = parseFloat(prompt("Please enter the fourth bill  amount:")); //Asking for fourth number to be placed into the array
