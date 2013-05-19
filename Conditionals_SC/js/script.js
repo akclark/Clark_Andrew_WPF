a24ce3dd126c23185f9de899adf29e7916cb6fab
Andrew Clark <akclark@fullsail.edu>
2013-05-19 16:38:52 -0400

    Conditional Logic - With an Expression work along

--- Conditionals_SC/js/script.js
+++ Conditionals_SC/js/script.js
@@ -1,9 +1,10 @@
-// Conditional Logic - Relational Expressions
-var kidHeight = 50;
-var minHeight = 48
+// Conditional Logic - With an Expression
+var kidHeight = 47;
+var minHeight = 48;
+var sneakerLift = 2;
 //if child is old enought,  print to console "you can ride!!"
 //if the kid is over 48 inches tall
-if(kidHeight > minHeight){
+if(kidHeight +sneakerLift > minHeight){
 	//code performed if condition is true
 	console.log("You can ride the coaster!!");
 	}
