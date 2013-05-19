ada2bfb8b1fbe7da88e8ba089c57c990d175df16
Andrew Clark <akclark@fullsail.edu>
2013-05-19 17:05:33 -0400

    Conditional Logic - Else If work along

--- Conditionals_SC/js/script.js
+++ Conditionals_SC/js/script.js
@@ -1,12 +1,15 @@
-// Conditional Logic - If and Else
-var kidHeight = 47;
-var minHeight = 48;
+// Conditional Logic - Else If
+var kidHeight = 46;
+var minHeight = 48; //if the kid is over 48 inches tall
+var parentHeight = 45; //the height of the kid with parent
 
-//if child is old enought,  print to console "you can ride!!"
-//if the kid is over 48 inches tall
+
 if(kidHeight > minHeight){
 	//code performed if condition is true
 	console.log("You can ride the coaster!!");
+	}else if(kidHeight > parentHeight){
+		//you can ride with a parent present
+		console.log("You can ride, but only with a parent present!");
 	}else{
 		//code performed if condition is false
 		console.log("Sorry kid, you've got some growing to do first!");
