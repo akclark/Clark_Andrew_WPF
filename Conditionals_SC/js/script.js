0e63a99f980f71b901438367125177c42a306fb6
Andrew Clark <akclark@fullsail.edu>
2013-05-19 17:46:32 -0400

    Conditional Logic - Logical Operators work along

--- Conditionals_SC/js/script.js
+++ Conditionals_SC/js/script.js
@@ -1,19 +1,12 @@
-// Conditional Logic - Else If
-var kidHeight = 46;
-var minHeight = 48; //if the kid is over 48 inches tall
-var parentHeight = 45; //the height of the kid with parent
+// Conditional Logic - Logical Operators
 
+var budget = 100;
+var iPhonePrice = 199.99;
+var wonLottery = true;
 
-if(kidHeight > minHeight){
-	//code performed if condition is true
-	console.log("You can ride the coaster!!");
-	}else if(kidHeight > parentHeight){
-		//you can ride with a parent present
-		console.log("You can ride, but only with a parent present!");
-	}else{
-		//code performed if condition is false
-		console.log("Sorry kid, you've got some growing to do first!");
-	}
-/*if(kidHeight <= minHeight){
-	console.log("Sorry kid, you've got some growing to do first!")
-}*/
+//if the price of the phone is less than out budget and if our check is over 300.
+if(iPhonePrice < budget || wonLottery){
+	console.log("We can buy the phone!");
+}else{
+	console.log("No phone for you!");
+}
