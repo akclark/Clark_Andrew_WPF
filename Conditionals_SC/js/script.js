e4336725654d7ddf781ebfe9e1cb020f4e52e852
Andrew Clark <akclark@fullsail.edu>
2013-05-19 18:19:32 -0400

    Conditional Logic - Ternary Operators walk along

--- Conditionals_SC/js/script.js
+++ Conditionals_SC/js/script.js
@@ -1,12 +1,34 @@
-// Conditional Logic - Logical Operators
+// Conditional Logic - Ternary Operators
 
-var budget = 100;
-var iPhonePrice = 199.99;
-var wonLottery = true;
+var age = 10;
+var book;
 
-//if the price of the phone is less than out budget and if our check is over 300.
-if(iPhonePrice < budget || wonLottery){
-	console.log("We can buy the phone!");
+//if the child is under 10, they get green eggs and ham, otherwise they get the time machine
+if(age <= 10){
+	book = "Green Eggs and Ham";
 }else{
-	console.log("No phone for you!");
+	book= "The Time Machine";
 }
+
+book = (age <= 10) ? "Green Eggs and Ham" : "The Time Machine";
+console.log(book);
+
+/*var gpa = 1.95;
+
+//if the gpa is over the min 2.0 score, the student can graduate
+if(gpa > 2.0){
+	console.log("You can graduate!");
+}else{
+	console.log("GPA is too low!");
+}
+
+(gpa > 2.0) ? console.log("You can graduate!") : console.log("GPA is too low!");*/
+
+/*if(condition){
+	do if true;
+}else{
+	do if false;
+}
+
+//ternary version of above statement
+(condition) ? do if true : do if false;*/
