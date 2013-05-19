b04edf37c5604963b429968820170d60a2ee3438
Andrew Clark <akclark@fullsail.edu>
2013-05-19 16:31:06 -0400

    Conditionals Example follow along

--- Conditionals_SC/js/script.js
+++ Conditionals_SC/js/script.js
@@ -1,11 +1,9 @@
 // JavaScript Document
-if (sunny){
-	goToTheBeach();
-	
-	if(warmWater){
-		wearNewSuit();
+var oldEnough = false;
+
+//if child is old enought,  print to console "you can ride!!"
+if(oldEnough){
+	//code performed if condition is true
+	console.log("You can ride the coaster!!");
 	}
-}
-else{
-	goToTheMovies();
-	}
+console.log("What comes after.");
