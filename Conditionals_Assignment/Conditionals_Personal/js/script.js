fdcfe4b6f340845d70e65ebf9e301f137b35109d
Andrew Clark <akclark@fullsail.edu>
2013-05-23 02:07:47 -0400

    Conditionals Personal - initial variable setup

--- Conditionals_Assignment/Conditionals_Personal/js/script.js
+++ Conditionals_Assignment/Conditionals_Personal/js/script.js
@@ -2,3 +2,5 @@
 
 //Weight converter, pounds to kilograms
 
+var weightType = prompt("Will you be entering lbs or kg?");
+var weight = prompt("Please enter the number of " + weightType + " you would like to convert:");
