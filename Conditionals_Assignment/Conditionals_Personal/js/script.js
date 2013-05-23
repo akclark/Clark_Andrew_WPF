ac084c19be36f3d083203bf5aec85a5a91cd2eef
Andrew Clark <akclark@fullsail.edu>
2013-05-23 02:18:55 -0400

    added comments and if/else for extra variables.

--- Conditionals_Assignment/Conditionals_Personal/js/script.js
+++ Conditionals_Assignment/Conditionals_Personal/js/script.js
@@ -2,5 +2,11 @@
 
 //Weight converter, pounds to kilograms
 
-var weightType = prompt("Will you be entering lbs or kg?");
-var weight = prompt("Please enter the number of " + weightType + " you would like to convert:");
+var weightType = prompt("Will you be entering lbs or kg?"); //asking user to input the first variable as type of weight
+if(weightType === "lbs"){ //so if user enters lbs
+	var weightTypeOpposite = "kg" //then var is set to kg
+}else{ //otherwise
+	weightTypeOpposite = "lbs" //it is set to lbs
+} //end statement
+var weight = prompt("Please enter the number of " + weightType + " you would like to convert to " + weightTypeOpposite + ":"); //asking user to input the amount of weight they want to convert
+
