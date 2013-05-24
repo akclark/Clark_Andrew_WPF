2b51c6ff2b5b98d8a41009092cf5201eca6a2e6a
Andrew Clark <akclark@fullsail.edu>
2013-05-23 22:18:56 -0400

    Initial file setup, plus description of what is going on, plus first variable setup and check

---
+++ Conditionals_Assignment/Conditionals_Wacky/js/script.js
@@ -0,0 +1,9 @@
+// Andrew K Clark	- Condtionals_Wacky - 23 May 2013
+
+// This will calculate the number of kittens (average of course) that your pet great horned owl weighs
+var owlLbs = prompt("How many lbs does your pet great horned owl weigh?");
+if (owlLbs == ""){
+	owlLbs = prompt("Please enter the number of lbs your owl weighs.");
+}
+
+
