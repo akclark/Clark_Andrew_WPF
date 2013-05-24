dcb4042eaa08c49f8a39a534865551936b091e0f
Andrew Clark <akclark@fullsail.edu>
2013-05-23 20:07:38 -0400

    Added a verification check for the second value to be sure something has been entered

--- Conditionals_Assignment/Conditionals_Personal/js/script.js
+++ Conditionals_Assignment/Conditionals_Personal/js/script.js
@@ -15,6 +15,10 @@ if(weightType === "lbs"){ //so if user enters lbs
 
 var weight = prompt("Please enter the number of " + weightType + " you would like to convert to " + weightTypeOpposite + ":"); //asking user to input the amount of weight they want to convert
 
+if(weight == ""){ //checking to see if something has been entered, don't know how to check if it's a number yet
+	weight = prompt("Please enter a number."); //Another prompt if the user has not entered a value
+} //end statement
+
 var factor = 2.20462; //added a var for the multiplier or divider
 var convertedWeight //created variable to be filled by calc
 (weightType === "kg") ? convertedWeight = weight * factor : convertedWeight = weight / factor; // calc ternary
