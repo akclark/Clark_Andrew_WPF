e9688f0b3cde2a5422665ebe6ac4b926eca4a88b
Andrew Clark <akclark@fullsail.edu>
2013-05-23 23:43:55 -0400

    Conditionals_Industry initial setup

---
+++ Conditionals_Assignment/Conditionals_Industry/js/script.js
@@ -0,0 +1,6 @@
+//Andrew K Clark - Conditionals_Industry - 23 May 2013
+
+//This will calculate the amount that website hosting, and domain name registering will cost for a year when split between family members
+
+var domainReg = prompt("How much per year does it cost you for domain name registration?");
+
