38f2d912261055e8e07fcca79d0b4266fb8b831b
Andrew Clark <akclark@fullsail.edu>
2013-05-28 01:28:20 -0400

    Anonymous Functions sub lesson

--- Functions_Screencast/js/script.js
+++ Functions_Screencast/js/script.js
@@ -1,18 +1,34 @@
 // Andrew K Clark - Screencast Work Along - 27 May 2013
 
-//Function vs Procedure
+//Anonymous Functions
 
-/* Procedure is more specific type of function
-- They never return values*/
+/*
+- Also called closures
+- They are functions that are created as the code is run
 
-//This is a function
-function calcArea(width, length){
+*/
+
+function functionName(){ //normal function setup
+}
+
+
+var functionName = function(){ //anonymous function setup
+	//code to run
+}
+
+functionName();
+//|||||||||||||||||||||||||||||||||||||||||||||
+
+//Anonymous functions must be defined before they can be invoked
+//Named functions do not, they are 'hoisted', which means they are created before the code is run
+
+var calcArea = function(width, length){
 	var area = width * length;
 	return area;
 }
 
-//This is a procedure
-function calcArea(width, length){
-	var area = width * length;
-	console.log(area) //no return
-}
+var a = calcArea(20, 30);
+
+console.log(a);
+
+//Anonymous is for code brevity, and variable scoping control
