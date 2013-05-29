f917965f922d48e6a89a69bce94ffe479d538ef6
Andrew Clark <akclark@fullsail.edu>
2013-05-29 17:50:13 -0400

    Moved console.log output to remind myself that the function does not have to be entered before my output.

--- Functions_Worksheet/js/script.js
+++ Functions_Worksheet/js/script.js
@@ -11,15 +11,16 @@ function calculateArea(width, length){ //function setup
 	return width * length;//doing the calc and returning the answer to the area variable
 } //end function
 
-//CIrcumference
+//Circumference
 
 var radius = prompt("What is the radius of the circle in question?");
 var pi = 3.14;
 var circumference = circumCalc(radius, pi);
+
+console.log("The circumference of the circle is " + circumference + ".");
 
 function circumCalc(radius, pi){
 	var circ = (radius * 2) * pi;
 	return circ
 }
 
-console.log("The circumference of the circle is " + circumference + ".");
