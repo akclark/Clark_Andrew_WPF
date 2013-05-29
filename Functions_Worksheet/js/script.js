0dc8195cebfef9240bda86cbaff8e7ac9887b65d
Andrew Clark <akclark@fullsail.edu>
2013-05-29 18:17:51 -0400

    Comment first line

--- Functions_Worksheet/js/script.js
+++ Functions_Worksheet/js/script.js
@@ -26,4 +26,4 @@ function circumCalc(radius, pi){ //initial function setup, with parameter setup
 
 //Stung!!
 
-var weight = prompt("How much does the victim weigh in lbs?");
+var weight = prompt("How much does the victim weigh in lbs?"); //the first variable and user input
