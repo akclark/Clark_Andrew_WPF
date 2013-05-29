0026257b51ef3769708bb047505255bce529a334
Andrew Clark <akclark@fullsail.edu>
2013-05-29 17:30:20 -0400

    Example function input

--- Functions_Worksheet/js/script.js
+++ Functions_Worksheet/js/script.js
@@ -1,2 +1,14 @@
-// JavaScript Document
-alert("This is in my JavaScript file.");
+//Andrew K Clark - Functions Worksheet - 29 May 2013
+
+//Example - Calculate the area of a Rectangle
+
+var width = 5;
+var length = 6;
+var area = calculateArea(width, length);
+console.log("The area of the rectangle is " + area + ".");
+
+function calculateArea(width, length)
+{
+	return width * length;
+}
+
