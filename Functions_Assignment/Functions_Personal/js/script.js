13b8c8c2b411f7fa5ee786912e520f7892e04ddc
Andrew Clark <akclark@fullsail.edu>
2013-05-30 01:37:40 -0400

    function experiment

--- Functions_Assignment/Functions_Personal/js/script.js
+++ Functions_Assignment/Functions_Personal/js/script.js
@@ -4,6 +4,32 @@
 
 var serverName = prompt("What is your server's name?");
 var billAmount = prompt("What is the total amount of your bill?");
-var tipPercent = prompt("What percentage of your bill would you like to leave for a tip?");
-var billSplit = prompt("How many people are splitting this bill? \nIf you are paying the whole thing, please enter 1.");
+var billNum = numCheck(billAmount);
+if(billNum === true){
+	billAmount = prompt("Please enter only numbers here.");
+}
 
+var tipPercent = prompt("What percentage of your bill would you like to leave for a tip? \n(Usually between 15% and 20%)");
+var tipNum = numCheck(tipPercent);
+if(tipNum === true){
+	tipPercent = prompt("Please enter only numbers here.");
+}
+
+var billSplit = prompt("How many people are splitting this bill? \nIf you are paying the whole thing, please enter 1.");
+var splitNum = numCheck(billSplit);
+if(splitNum === true){
+	billSplit = prompt("Please enter only numbers here.");
+}
+
+
+
+
+
+
+
+
+function numCheck(string){
+	var num = isNaN(string);
+	return num;
+}
+		
