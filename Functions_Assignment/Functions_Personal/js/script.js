147335f1366a47c75e1297a9fd57e1dd0b6d6a2a
Andrew Clark <akclark@fullsail.edu>
2013-05-29 23:02:33 -0400

    Initial variable setup

--- Functions_Assignment/Functions_Personal/js/script.js
+++ Functions_Assignment/Functions_Personal/js/script.js
@@ -1,2 +1,9 @@
-// JavaScript Document
-alert("This is in my JavaScript file.");
+// Andrew K Clark - Functions Personal - 29 May 2013
+
+//Tip Calculator
+
+var serverName = prompt("What is your server's name?");
+var billAmount = prompt("What is the total amount of your bill?");
+var tipPercent = prompt("What percentage of your bill would you like to leave for a tip?");
+var billSplit = prompt("How many people are splitting this bill? \nIf you are paying the whole thing, please enter 1.");
+
