2a0a1cd2842f282cf800af46c025dc8e98a97bb3
Andrew Clark <akclark@fullsail.edu>
2013-05-30 23:04:30 -0400

    added blank input checks for some variables and some comments

--- Functions_Assignment/Functions_Personal/js/script.js
+++ Functions_Assignment/Functions_Personal/js/script.js
@@ -3,13 +3,23 @@
 //Tip Calculator
 
 var serverName = prompt("What is your server's name?");
+if(serverName == ""){ //checking to see if something has been entered
+	serverName = prompt("Please enter your server's name."); //Another prompt if the user has not entered a value
+} //end statement
+
 var billAmount = prompt("What is the total amount of your bill?");
+if(billAmount == ""){ //checking to see if something has been entered, don't know how to check if it's a number yet
+	billAmount = prompt("Please enter the total amount of your bill."); //Another prompt if the user has not entered a value
+} //end statement
 var billNum = numCheck(billAmount);
 if(billNum === true){
 	billAmount = prompt("Please enter only numbers here.");
 }
 
 var tipPercent = prompt("What percentage of your bill would you like to leave for a tip? \n(Usually between 15% and 20%)");
+if(tipPercent == ""){ //checking to see if something has been entered, don't know how to check if it's a number yet
+	tipPercent = prompt("Please enter the percentage you would like to leave for a tip."); //Another prompt if the user has not entered a value
+} //end statement
 var tipNum = numCheck(tipPercent);
 if(tipNum === true){
 	tipPercent = prompt("Please enter only numbers here.");
