022a4dbd1db005c811d4c5cc68391d82fd5ca97e
Andrew Clark <akclark@fullsail.edu>
2013-05-30 23:59:43 -0400

    beginning initial var setup

--- Functions_Assignment/Functions_Industry/js/script.js
+++ Functions_Assignment/Functions_Industry/js/script.js
@@ -1,2 +1,5 @@
-// JavaScript Document
-alert("This is in my JavaScript file.");
+// Andrew K Clark - Functions Industry - 30 May 2013
+
+//voltage drop calculator
+
+var wireGauge = prompt("Please enter the size of your wire:");
