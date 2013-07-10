9733201e863eee0ec1976c4fa69ae1fcad8b8208
Andrew Clark <akclark@fullsail.edu>
2013-07-10 16:55:03 -0400

    no message

--- Expressions_Assignment/Expressions_Wacky/js/script.js
+++
@@ -1,1 +0,0 @@
-// Andrew K Clark - 05/16/2013 - Expressions Wacky
--- Functions_Assignment/Functions_Industry/js/script.js
+++
@@ -1,5 +0,0 @@
-// Andrew K Clark - Functions Industry - 30 May 2013
-
-//voltage drop calculator
-
-var wireGauge = prompt("Please enter the size of your wire:");
