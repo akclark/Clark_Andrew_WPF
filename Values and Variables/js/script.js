876676a7bac89a43aa5c98a8df8b11b68152c64c
Andrew Clark <akclark@fullsail.edu>
2013-07-14 22:24:30 -0400

    Console.log and alert work along file

--- Values and Variables/js/script.js
+++ Values and Variables/js/script.js
@@ -1,2 +1,10 @@
 //Values and Variables Workalong
 //Andrew K Clark - 14 July 2013
+//Using console.log and alert
+
+var yearBorn = 1955;
+ 
+ //console.log("Hello World!!");
+ //console.log(yearBorn);
+ 
+ alert(yearBorn);
