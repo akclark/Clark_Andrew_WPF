cc18ae170152ececa39fe84005dbe0b1f10e9b72
Andrew Clark <akclark@fullsail.edu>
2013-07-14 22:33:33 -0400

    Declaring variables work along

--- Values and Variables/js/script.js
+++ Values and Variables/js/script.js
@@ -1,10 +1,5 @@
 //Values and Variables Workalong
 //Andrew K Clark - 14 July 2013
-//Using console.log and alert
+//Declaring Variables
 
-var yearBorn = 1955;
- 
- //console.log("Hello World!!");
- //console.log(yearBorn);
- 
- alert(yearBorn);
+var age;
