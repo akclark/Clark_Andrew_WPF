a7b4af9c03c26bbb2ab85016ccca680f8616d739
Andrew Clark <akclark@fullsail.edu>
2013-07-17 08:45:26 -0400

    Asked for user input of first variable for calc

--- Expressions Assignment/Expressions_Industry/js/script.js
+++ Expressions Assignment/Expressions_Industry/js/script.js
@@ -2,5 +2,6 @@
 //15 July 2013
 //Expressions - Industry
 
-//Hi there
-alert("Hello World!!!");
+//This will calculate the number of man hours it will take to wire a given number of apartment units.
+
+var numberOfUnits = prompt("Please enter the number of units total that need to be wired:");
