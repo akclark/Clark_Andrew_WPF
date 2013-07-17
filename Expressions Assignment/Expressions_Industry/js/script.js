32cf9f686ba789686d405e1dda1ac60d4350fd82
Andrew Clark <akclark@fullsail.edu>
2013-07-17 08:57:40 -0400

    Added query for second variable.

--- Expressions Assignment/Expressions_Industry/js/script.js
+++ Expressions Assignment/Expressions_Industry/js/script.js
@@ -5,3 +5,6 @@
 //This will calculate the number of man hours it will take to wire a given number of apartment units.
 
 var numberOfUnits = prompt("Please enter the number of units total that need to be wired:");
+
+var numberOfMen = prompt("Please enter the number of workers required to wire one (1) unit:");
+
