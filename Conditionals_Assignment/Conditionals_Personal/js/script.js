af5cf12206cbfe4626e10f91678b519c4b7cc1c1
Andrew Clark <akclark@fullsail.edu>
2013-07-25 12:36:38 -0400

    Added some comments

--- Conditionals_Assignment/Conditionals_Personal/js/script.js
+++ Conditionals_Assignment/Conditionals_Personal/js/script.js
@@ -7,4 +7,5 @@
 var volumeType = prompt("Will you be entering quarts or litres?"); //asking for input of the first variable as a type of volume
 if (volumeType != "quarts" && volumeType != "litres") {
     volumeType = prompt("Please enter either quarts or litres!")
-}
+} //added logical operator and conditional to check for proper user input
+
