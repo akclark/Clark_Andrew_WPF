d2b30b6d67c5b543bcbcb3278c476820285cfef4
Andrew Clark <akclark@fullsail.edu>
2013-07-25 22:14:37 -0400

    wrote out request for first user input of variable, as well as a check for blank entry.

--- Conditionals_Assignment/Conditionals_Industry/js/script.js
+++ Conditionals_Assignment/Conditionals_Industry/js/script.js
@@ -1,1 +1,10 @@
-alert("Hello World!");
+//Andrew K Clark
+//Industry Conditionals Assignment
+//25 July 2013
+
+//Pixels to inches conversion
+
+var pixelHeight =  prompt("How many pixels tall is your image?");
+if(pixelHeight == "") {
+    pixelHeight = prompt("Please enter the number of pixels tall your image is.")
+}
