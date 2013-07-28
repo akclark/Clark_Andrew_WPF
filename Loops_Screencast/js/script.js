1efc68fbb3d65c481573fdec35275e7e585021a2
Andrew Clark <akclark@fullsail.edu>
2013-07-27 23:33:20 -0400

    loops screencast lynda.com video work along

--- Loops_Screencast/js/script.js
+++ Loops_Screencast/js/script.js
@@ -24,3 +24,52 @@ while (c > 0);
 for (var = 10; i > 0; i--) {
     console.log(i + "kegs on the wall.");
 }
+
+//Lynda.com video workalong
+
+var a = 1;
+
+while (a < 10) {
+    console.log(a);
+    a++;
+}
+
+//do-while
+var a = 1;
+
+do {
+    console.log(a);  //This block will always run at least once
+    a++;
+} while (a < 10);
+
+//for loop
+
+for (var i = 1; i < 10; i++) {
+    //do stuff
+    //do stuff
+    //etc...
+}
+
+// Break
+
+for (var i = 1; i < 5000; i++) {
+    //do stuff
+    if (i == 101) {
+        break;
+    }
+    //do stuff
+}
+
+// break jumps out of the loop
+
+//continue - moves us back to the top of the loop
+
+for ( var i = 1; i < 5000; i++;) {
+    //do stuff
+    //do stuff
+    if (i % 5 == 0) {
+        continue;
+    }
+    //do second set of stuff
+    //do second set of stuff
+}
