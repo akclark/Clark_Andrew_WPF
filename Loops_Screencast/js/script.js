1d16a403c929c07b943b4e8a0f8c1d0cd5180a6f
Andrew Clark <akclark@fullsail.edu>
2013-07-27 23:12:29 -0400

    do - while loop video work along

--- Loops_Screencast/js/script.js
+++ Loops_Screencast/js/script.js
@@ -8,3 +8,13 @@ while (b > 0) { //checks the condition
     console.log(b + " kegs on the wall.");
     b--;//increments or decrements the index.
 }
+
+//do-while loops
+
+var c = 10;
+
+do{ //This block will still be run even if the while protion that follows is not true
+    console.log(c + " kegs on the wall.");
+    c--;
+}
+while (c > 0); 
