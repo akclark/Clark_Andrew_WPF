7dcfc9a0f0d4d4b3eaa49fd56c40a3817117f11e
Andrew Clark <akclark@fullsail.edu>
2013-07-31 14:45:56 -0400

    Basic function Structure work along.

--- Functions_Screencast/js/script.js
+++ Functions_Screencast/js/script.js
@@ -6,3 +6,22 @@
 
 //Functions store stuff like variables do, however variables store values, while functions store blocks of code.
 //Functions allow you to have the code in one place and run that code as many times as you want.
+
+//Basic function structure:
+
+function functionName () {
+    //code the function runs
+}
+//parenthesis are for parameters
+//functions have names so we can refer to them later
+
+function outputMsg() {
+    console.log("Hello World!");
+}
+
+function calcArea() {
+    var width = 20;
+    var length = 30;
+    var area = width * length;
+    console.log(area);
+}
