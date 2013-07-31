3c96c8c4d61dc59022316079d9cdc721b9edda86
Andrew Clark <akclark@fullsail.edu>
2013-07-31 14:37:46 -0400

    Introduction to functions notes and work long.

--- Functions_Screencast/js/script.js
+++ Functions_Screencast/js/script.js
@@ -1,1 +1,8 @@
-alert("Hello World!");
+//Andrew K. Clark
+//31 July 2013
+//Functions - Screencast work along.
+
+//Intro to Functions
+
+//Functions store stuff like variables do, however variables store values, while functions store blocks of code.
+//Functions allow you to have the code in one place and run that code as many times as you want.
