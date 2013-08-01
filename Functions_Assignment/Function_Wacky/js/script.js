2c3e09a43dcc43e2508a9b32574500d8efb1af2d
Andrew Clark <akclark@fullsail.edu>
2013-08-01 17:05:57 -0400

    File is done, comments and all.

--- Functions_Assignment/Function_Wacky/js/script.js
+++ Functions_Assignment/Function_Wacky/js/script.js
@@ -4,39 +4,23 @@
 
 //This will calculate the really ridiculous number of diapers that you will use in a month, and across six months for a newborn, based on the average number used per day.  Assuming 30 days average per month.
 
-var averagePerDay = prompt("Please enter the average number of diapers per day your newborn goes through:");
-var numberOfMonths = prompt("Please enter the number of months you wish to calculate for:");
+var averagePerDay = prompt("Please enter the average number of diapers per day your newborn goes through:");//First variable request
+var numberOfMonths = prompt("Please enter the number of months you wish to calculate for:");//second variable request
 
-if(averagePerDay == "" || numberOfMonths == "") {
-    var averagePerDay = prompt("Please enter the average number of diapers per day your newborn goes through:");
-    var numberOfMonths = prompt("Please enter the number of months you wish to calculate for:");
+if(averagePerDay == "" || numberOfMonths == "") {//checking to see if either of the inputs were blanks
+    var averagePerDay = prompt("Please enter the average number of diapers per day your newborn goes through:");//requesting again if blank
+    var numberOfMonths = prompt("Please enter the number of months you wish to calculate for:");//requesting again if blannk
     
-    totalAcrossMonths = totalMonths(averagePerDay, numberOfMonths);
-    console.log("The total number of diapers you will use across " + numberOfMonths + " months is: " + totalAcrossMonths + ".");
-    totalPerMonth = perMonthCalc(averagePerDay, numberOfMonths);
-    console.log("Which is " + totalPerMonth + " per month.");
-}else{
-    totalAcrossMonths = totalMonths(averagePerDay, numberOfMonths);
-    console.log("The total number of diapers you will use across " + numberOfMonths + " months is: " + totalAcrossMonths + ".");
-    totalPerMonth = perMonthCalc(averagePerDay, numberOfMonths);
-    console.log("Which is " + totalPerMonth + " per month.");
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    totalAcrossMonths = totalMonths(averagePerDay, numberOfMonths);//invoking first function
+    console.log("The total number of diapers you will use across " + numberOfMonths + " months is: " + totalAcrossMonths + ".");//required console.log output
+    totalPerMonth = perMonthCalc(averagePerDay, numberOfMonths);//calling second function
+    console.log("Which is " + totalPerMonth + " per month.");//second function console.log output
+}else{//do this if the user actually managed to input 2 numbers
+    totalAcrossMonths = totalMonths(averagePerDay, numberOfMonths);//first function call
+    console.log("The total number of diapers you will use across " + numberOfMonths + " months is: " + totalAcrossMonths + ".");//required console.log output
+    totalPerMonth = perMonthCalc(averagePerDay, numberOfMonths);//second function call
+    console.log("Which is " + totalPerMonth + " per month.");//second function console.log output
+}//end statement
 
 
 //Functions
