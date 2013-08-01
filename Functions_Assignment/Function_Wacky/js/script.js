ac9e2b02630e447689a1216434baa30213be8189
Andrew Clark <akclark@fullsail.edu>
2013-08-01 16:41:37 -0400

    added requests for first two variables

--- Functions_Assignment/Function_Wacky/js/script.js
+++ Functions_Assignment/Function_Wacky/js/script.js
@@ -1,1 +1,28 @@
-alert("Hello World!");
+//Andrew K Clark
+//01 August 2013
+//Function Wacky
+
+//This will calculate the really ridiculous number of diapers that you will use in a month, and across six months for a newborn, based on the average number used per day.  Assuming 30 days average per month.
+
+var averagePerDay = prompt("Please enter the average number of diapers per day your newborn goes through:");
+var numberOfMonths = prompt("Please enter the number of months you wish to calculate for:");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
