9dfcd3dec4ce46c6357ecf321fc030526d5b7c0e
Andrew Clark <akclark@fullsail.edu>
2013-08-01 22:10:32 -0400

    added a check on the first numerical variable to see if the user entered a number

--- Functions_Assignment/Function_Personal/js/script.js
+++ Functions_Assignment/Function_Personal/js/script.js
@@ -10,5 +10,10 @@ if (servNam == "") {//checking for blank input
 }//end statement
 
 var billAmount = prompt("What was the bill total?");//asking for first calc variable
+var numBill = isNaN(billAmount);//checking to see if the user entered a number
+if (numBill === true) {//if they didn't
+    billAmount = prompt("Please enter an actual number here:");//we ask them to do so
+}//end statement
+
 var percentTip = prompt("Please enter the percentage of your bill that you would like to leave for a tip. \nCommonly between 15% and 20%.");//asking for second calc variable
 var splitNum = prompt("How any people are splitting this bill?");//asking for third calc variable
